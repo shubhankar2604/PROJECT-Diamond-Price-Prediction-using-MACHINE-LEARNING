c5424920eb48f0c13bf311e7538ff936d6ff86a1
Shubhankar Bhattacharya <114163915+shubhankar2604@users.noreply.github.com>
2023-09-30 18:49:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DiamondPricePrediction',
     version='0.0.1',
-    author='Krish',
-    author_email='krish.naik@pw.live',
+    author='Shubhankar',
+    author_email='shubhankar2604@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
